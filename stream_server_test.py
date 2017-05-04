d4c97fcf01ee6736a41967b459e06a4e71237ab7
EAZYE9000 <rocketmanpocketman@gmail.com>
2017-05-04 21:01:38 +0100

    Update stream_server_test.py

--- stream_server_test.py
+++ stream_server_test.py
@@ -9,15 +9,16 @@ import sys
 e1 = cv2.getTickCount()
 image_array = np.zeros((1, 38400))
 label_array = np.zeros((1, 4), 'float')
+label_array_2 = np.zeros((1, 4), 'float')
 
 server_socket = socket.socket()
-server_socket.bind(('192.168.1.105', 8000))
+server_socket.bind(('192.168.1.85', 8000))
 server_socket.listen(0)
 connection, client_address = server_socket.accept()
 connection = connection.makefile('rb')
 
 print("Server Up")
-serial_port = serial.Serial('COM5', 9600, timeout=1)
+serial_port = serial.Serial('COM4', 9600, timeout=1)
 
 def write(int):
     return serial_port.write(chr(int))
@@ -38,6 +39,7 @@ for i in range(4):
     temp_label = np.zeros((1, 4), 'float')
 
 stream_bytes = ' '
+counter = 0
 while True:
     stream_bytes += connection.read(1024)
     first = stream_bytes.find('\xff\xd8')
@@ -45,11 +47,11 @@ while True:
     if first != -1 and last != -1:
         jpg = stream_bytes[first:last + 2]
         stream_bytes = stream_bytes[last + 2:]
-        #image = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.CV_LOAD_IMAGE_GRAYSCALE)
         image = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
         roi = image[120:240, :]
-        cv2.imshow('image', image)
+        #cv2.imshow('image', image)
         temp_array = roi.reshape(1, 38400).astype(np.float32)
+        counter += 1
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -58,17 +60,25 @@ while True:
         if event.type == KEYDOWN:
             key_input = pygame.key.get_pressed()
 
+            jpg = stream_bytes[first:last + 2]
+            stream_bytes = stream_bytes[last + 2:]
+            image = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
+
             if key_input[pygame.K_UP] and key_input[pygame.K_RIGHT]:
                 print("Forward Right")
                 saved_frame +=1
                 write(5)
                 image_array = np.vstack((image_array, temp_array))
                 label_array = np.vstack((label_array, k[1]))
+                cv2.imwrite('dataset_images/image{:>05}.jpg'.format(counter), image)
+                label_array_2 = np.vstack((label_array_2, k[1]))
             elif key_input[pygame.K_UP] and key_input[pygame.K_LEFT]:
                 saved_frame += 1
                 write(6)
                 image_array = np.vstack((image_array, temp_array))
                 label_array = np.vstack((label_array, k[0]))
+                cv2.imwrite('dataset_images/image{:>05}.jpg'.format(counter), image)
+                label_array_2 = np.vstack((label_array_2, k[0]))
             elif key_input[pygame.K_DOWN] and key_input[pygame.K_RIGHT]:
                 saved_frame += 1
                 write(7)
@@ -80,21 +90,29 @@ while True:
                 write(1)
                 image_array = np.vstack((image_array, temp_array))
                 label_array = np.vstack((label_array, k[2]))
+                cv2.imwrite('dataset_images/image{:>05}.jpg'.format(counter), image)
+                label_array_2 = np.vstack((label_array_2, k[2]))
             elif key_input[pygame.K_DOWN]:
                 saved_frame += 1
                 write(2)
                 image_array = np.vstack((image_array, temp_array))
                 label_array = np.vstack((label_array, k[3]))
+                cv2.imwrite('dataset_images/image{:>05}.jpg'.format(counter), image)
+                label_array_2 = np.vstack((label_array_2, k[3]))
             elif key_input[pygame.K_RIGHT]:
                 saved_frame += 1
                 write(3)
                 image_array = np.vstack((image_array, temp_array))
                 label_array = np.vstack((label_array, k[1]))
+                cv2.imwrite('dataset_images/image{:>05}.jpg'.format(counter), image)
+                label_array_2 = np.vstack((label_array_2, k[1]))
             elif key_input[pygame.K_LEFT]:
                 saved_frame += 1
                 write(4)
                 image_array = np.vstack((image_array, temp_array))
                 label_array = np.vstack((label_array, k[0]))
+                cv2.imwrite('dataset_images/image{:>05}.jpg'.format(counter), image)
+                label_array_2 = np.vstack((label_array_2, k[0]))
             elif key_input[pygame.K_x] or key_input[pygame.K_q] or key_input[pygame.K_ESCAPE]:
                 print("Exit")
                 write(0)
@@ -105,8 +123,9 @@ while True:
 
     train = image_array[1:, :]
     train_labels = label_array[1:, :]
+    train_labels_2 = label_array_2[1:, :]
 
-    np.savez('test_dataset.npz', train=train, train_labels=train_labels)
+    np.savez('rc_car_dataset.npz', train=train, train_labels=train_labels, train_labels_2=train_labels_2)
     e2 = cv2.getTickCount()
     # calculate streaming duration
     #time0 = (e2 - e1) / cv2.getTickFrequency()
